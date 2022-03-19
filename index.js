8c56c5055279d3fae23cf4a7f9ced524894b29b6
Jason Antman <antmanjt@gmail.com>
2022-03-18 23:09:33 -0400

    commit tree e819b5aa782500f1b67d5213e6acc4871f63dd61

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 32;
+const num4 = 8;
+const mod = num3 / num4;
+
+const max = (1, 5, 10, 20)
+Math.max();
 // Write your code here
